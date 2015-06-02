643b7f4baff5bb6644c8cb72c4f857bb96d41d57
Alex Gondek <alexgondek@gmail.com>
2015-06-02 15:56:45 -0700

    Added links to view services

--- client/dashboard.js
+++ client/dashboard.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   var app = angular.module('pagerdutyDashboard', ['ngRoute']);
 
   var globalStatus = '';
@@ -7,7 +7,7 @@
   Routing Configuration
   */
 
-  app.config(function ($routeProvider) {
+  app.config(function($routeProvider) {
     $routeProvider
     .when('/', {
       templateUrl: 'assets/dashboard.html'
@@ -25,48 +25,48 @@
   Main Controller
   */
 
-  app.controller('appController', function ($scope, noty, socket, $routeParams) {
+  app.controller('appController', function($scope, noty, socket, $routeParams) {
     var timeoutWarning;
     function serverWarningReset() {
       var SECONDS = 30;
-      if(timeoutWarning !== undefined) {
+      if (timeoutWarning !== undefined) {
         clearTimeout(timeoutWarning);
       }
       noty.clear();
-      timeoutWarning =  setTimeout(function () {
-        noty.update('warning',
-          'There have been no updates from the server for ' + SECONDS + ' seconds.');
+      timeoutWarning =  setTimeout(function() {
+        noty.update('warning', 'The server has not sent updates in the last ' +
+                    SECONDS + ' seconds.');
       }, 1000 * SECONDS);
     }
     serverWarningReset();
 
     $scope.loaded = false;
 
-    $scope.getUiSettings = function () {
+    $scope.getUiSettings = function() {
       var classes = globalStatus;
-      if($routeParams.animatepage !== 'false') {
+      if ($routeParams.animatepage !== 'false') {
         classes += ' animate-background';
       }
-      if($routeParams.animateheadings === 'true') {
+      if ($routeParams.animateheadings === 'true') {
         classes += ' animate-headings';
       }
       return classes;
     };
 
-    socket.on('error', function (data) {
+    socket.on('error', function(data) {
       serverWarningReset();
       noty.update('warning', 'Error communicating with PagerDuty: ' + data);
     });
 
-    socket.on('update', function (data) {
+    socket.on('update', function(data) {
       serverWarningReset();
-      if($scope.hash === data.hash) {
+      if ($scope.hash === data.hash) {
         return;
       }
       $scope.hash = data.hash;
       $scope.loaded = true;
       $scope.data = data;
-      if(!$scope.cachedData) {
+      if (!$scope.cachedData) {
         // cache the inital update to avoid flickering on customization page
         $scope.cachedData = data;
       }
@@ -78,9 +78,9 @@
   Secondary Controllers
   */
 
-  app.controller('customizationController', function ($location, $scope) {
-    var baseUrl = $location.protocol() + "://" + $location.host();
-    baseUrl += ":" + $location.port() + '/#/?';
+  app.controller('customizationController', function($location, $scope) {
+    var baseUrl = $location.protocol() + '://' + $location.host();
+    baseUrl += ':' + $location.port() + '/#/?';
 
     $scope.querymode = 'includeall';
     $scope.queryGroups = {};
@@ -89,32 +89,34 @@
     $scope.animateHeadings = false;
     $scope.animatePage = true;
 
-    $scope.getUrl = function () {
-      var url = baseUrl, key, selected = [], groups = $scope.queryGroups;
+    $scope.getUrl = function() {
+      var url = baseUrl;
+      var selected = [];
+      var groups = $scope.queryGroups;
 
-      for (key in groups) {
+      for (var key in groups) {
         if (groups.hasOwnProperty(key) && groups[key]) {
           selected.push(key);
         }
       }
 
-      if('include' === $scope.querymode) {
+      if ('include' === $scope.querymode) {
         url += 'include=' + selected.join() + '&';
       } else if ('exclude' === $scope.querymode) {
         url += 'exclude=' + selected.join() + '&';
       }
 
-      if(!$scope.otherProducts) {
+      if (!$scope.otherProducts) {
         url += 'otherproducts=false&';
       }
-      if(!$scope.otherIssues) {
+      if (!$scope.otherIssues) {
         url += 'otherissues=false&';
       }
 
-      if($scope.animateHeadings) {
+      if ($scope.animateHeadings) {
         url += 'animateheadings=true&';
       }
-      if(!$scope.animatePage) {
+      if (!$scope.animatePage) {
         url += 'animatepage=false&';
       }
 
@@ -126,32 +128,41 @@
   Custom filters
   */
 
-  app.filter('filterGroups', function ($routeParams) {
+  app.filter('filterGroups', function($routeParams) {
     function compareGroups(a, b) {
-      if(a.status === b.status) {
+      if (a.status === b.status) {
         return a.features.length > b.features.length ? -1 : 1;
       }
       return a.statusNumber > b.statusNumber ? -1 : 1;
     }
 
-    return function (groups) {
-      var otherProducts = $routeParams.otherproducts !== 'false';
-      var otherIssues = $routeParams.otherissues !== 'false';
-      var queryMode = $routeParams.include !== undefined ? 'include' : 'exclude';
-      var queryList = $routeParams[queryMode] ? $routeParams[queryMode].split(',') : [];
-      var offCore = [], offOther = [], onCore = [], onOther = [];
-      if(!groups) {
+    return function(groups) {
+      if (!groups) {
         return groups;
       }
-      groups.forEach(function (group) {
-        if(group.name === "Other Products") {
+
+      var otherProducts = $routeParams.otherproducts !== 'false';
+      var otherIssues = $routeParams.otherissues !== 'false';
+      var queryMode = $routeParams.include !== undefined ?
+        'include' :
+        'exclude';
+      var queryList = $routeParams[queryMode] ?
+        $routeParams[queryMode].split(',') :
+        [];
+      var offCore = [];
+      var offOther = [];
+      var onCore = [];
+      var onOther = [];
+
+      groups.forEach(function(group) {
+        if (group.name === 'Other Products') {
           onOther = otherProducts ? group : [];
-        } else if (group.name === "Other Issues") {
+        } else if (group.name === 'Other Issues') {
           offOther = otherIssues ?  group : [];
         } else {
-          if((queryMode === 'exclude' && queryList.indexOf(group.id) === -1) ||
+          if ((queryMode === 'exclude' && queryList.indexOf(group.id) === -1) ||
            (queryMode === 'include' && queryList.indexOf(group.id) !== -1)) {
-            if(group.isOnline) {
+            if (group.isOnline) {
               onCore.push(group);
             } else {
               offCore.push(group);
@@ -175,13 +186,13 @@
   Custom factory services
   */
 
-  app.factory('socket', function ($rootScope) {
+  app.factory('socket', function($rootScope) {
     var socket = io.connect();
     return {
-      on: function (eventName, callback) {
-        socket.on(eventName, function () {
+      on: function(eventName, callback) {
+        socket.on(eventName, function() {
           var args = arguments;
-          $rootScope.$apply(function () {
+          $rootScope.$apply(function() {
             callback.apply(socket, args);
           });
         });
@@ -189,11 +200,11 @@
     };
   });
 
-  app.factory('noty', function () {
+  app.factory('noty', function() {
     var current;
     return {
-      update: function (type, message) {
-        if(!current || message !== current.options.text || current.closed) {
+      update: function(type, message) {
+        if (!current || message !== current.options.text || current.closed) {
           $.noty.closeAll();
           current = noty({
             text: message,
@@ -210,7 +221,7 @@
   Custom directives
   */
 
-  app.directive('service', function () {
+  app.directive('service', function() {
     return {
       restrict: 'E',
       templateUrl: 'assets/service.html'
