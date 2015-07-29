80d482990ec7989cf2eb3235a554d8edebca2c3b
Alex Gondek <alexgondek@gmail.com>
2015-07-29 15:17:13 -0700

    Added basic sort check to dashboard view test

--- test/spec.dashboardView.js
+++ test/spec.dashboardView.js
@@ -1,80 +1,81 @@
 describe('Dashboard View', function() {
 
-  it('should have 5 groups by default mock data', function() {
-    getUrl();
-    expect(groupRepeater().count()).toEqual(5);
+  it('should correctly sort the 5 default mock groups', function() {
+    dashboardUrl();
+    groupRepeater().all(by.css('h2 span')).getText().then(function(groups) {
+      expect(groups).toEqual([
+        'UnreliableSite',
+        'Other Issues',
+        'UnstableSite',
+        'StableSite',
+        'Other Products'
+      ]);
+    });
   });
 
   it('should display no groups when the cutoff is too high', function() {
-    getUrl('orderCutoff=1');
+    dashboardUrl('orderCutoff=1');
     expect(groupRepeater().count()).toEqual(0);
   });
 
   it('should display a message when there are no groups', function() {
-    getUrl('orderCutoff=1');
+    dashboardUrl('orderCutoff=1');
     expect(element(by.css('.no-groups')).isDisplayed()).toBeTruthy();
   });
 
   it('can have a flashing background by default', function() {
-    getUrl();
+    dashboardUrl();
     expect(bodyCssClass()).toMatch('animate-background');
   });
 
   it('should not have a flashing background when disabled', function() {
-    getUrl('animatePage=false');
+    dashboardUrl('animatePage=false');
     expect(bodyCssClass()).not.toMatch('animate-background');
   });
 
   it('can have flashing headers when enabled', function() {
-    getUrl('animateHeadings=true');
+    dashboardUrl('animateHeadings=true');
     expect(bodyCssClass()).toMatch('animate-headings');
   });
 
   it('should not have flashing headers by default', function() {
-    getUrl();
+    dashboardUrl();
     expect(bodyCssClass()).not.toMatch('animate-headings');
   });
 
   it('should allow flashing headers on and flashing background off at the same time', function() {
-    getUrl('animateHeadings=true&animatePage=false');
+    dashboardUrl('animateHeadings=true&animatePage=false');
     expect(bodyCssClass()).not.toMatch('animate-background');
     expect(bodyCssClass()).toMatch('animate-headings');
   });
 
   it('should have a global "critical" state by default mock data', function() {
-    getUrl();
+    dashboardUrl();
     expect(bodyCssClass()).toMatch('critical');
   });
 
   it('should have a global "warning" state when the mock "critical" services are hidden', function() {
-    getUrl('order-unreliablesite=-1&order-other-issues=-1');
+    dashboardUrl('order-unreliablesite=-1&order-other-issues=-1');
     expect(bodyCssClass()).toMatch('warning');
     expect(bodyCssClass()).not.toMatch('critical');
   });
 
   it('should have 3 groups when the mock "critical" services are hidden', function() {
-    getUrl('order-unreliablesite=-1&order-other-issues=-1');
+    dashboardUrl('order-unreliablesite=-1&order-other-issues=-1');
     expect(groupRepeater().count()).toEqual(3);
   });
 
   it('should have a global "active" state when the failing services are hidden', function() {
-    getUrl('order-other-issues=-1&order-unstablesite=-1&order-unreliablesite=-1&');
+    dashboardUrl('order-other-issues=-1&order-unstablesite=-1&order-unreliablesite=-1&');
     expect(bodyCssClass()).toMatch('active');
     expect(bodyCssClass()).not.toMatch('critical');
     expect(bodyCssClass()).not.toMatch('warning');
   });
 
   it('should have 2 groups when the mock "critical" & "warning" services are hidden', function() {
-    getUrl('order-other-issues=-1&order-unstablesite=-1&order-unreliablesite=-1&');
+    dashboardUrl('order-other-issues=-1&order-unstablesite=-1&order-unreliablesite=-1&');
     expect(groupRepeater().count()).toEqual(2);
   });
-
-  it('should correctly sort the default mock groups', function() {
-    getUrl();
-    expect(bodyCssClass()).not.toMatch('animate-headings');
-  });
-
-  // ordering
 
 });
 
@@ -86,6 +87,6 @@ function groupRepeater() {
   return element.all(by.repeater('group in groups'));
 }
 
-function getUrl(append) {
+function dashboardUrl(append) {
   browser.get('http://localhost:3000/dashboards/pagerduty/#/?' + (append || ''));
 }
