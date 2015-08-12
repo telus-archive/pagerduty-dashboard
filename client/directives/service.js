e4690b6c74dec757f37f2f5c5b5a95f1b96474c8
Alex Gondek <alexgondek@gmail.com>
2015-08-12 11:56:33 -0700

    Added title attribute on services with a value of the current status

--- client/directives/service.js
+++ client/directives/service.js
@@ -5,6 +5,12 @@ app.directive('service', function(dashboardSettings) {
     templateUrl: 'partials/service.html',
     scope: {
       service: '='
+    },
+    link: function(scope) {
+      scope.properCase = function(string) {
+        string = string || '';
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      };
     }
   };
 });
