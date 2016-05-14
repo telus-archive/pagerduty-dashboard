9bcd189134ea25d35b54265b5637934f154f6fe3
Alex Gondek <alexgondek@gmail.com>
2016-05-13 22:01:59 -0700

    Added protractor testing to travis CI

--- test/protractor.js
+++ test/protractor.js
@@ -1,4 +1,15 @@
-exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['spec/*.js']
+var config = {
+  specs: ['protractor/*.js']
 };
+
+if (process.env.TRAVIS) {
+  config.sauceUser = process.env.SAUCE_USERNAME;
+  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+  config.capabilities = {
+    'browserName': 'chrome',
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+    'build': process.env.TRAVIS_BUILD_NUMBER
+  };
+}
+
+exports.config = config;
